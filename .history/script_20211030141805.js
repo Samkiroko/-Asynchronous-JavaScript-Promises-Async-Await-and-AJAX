94e86f9b273d0f12a93db78b578e7ec5fb1d930a
Samkiroko <kirokodev@gmail.com>
2021-10-30 14:55:07 +0300

    notes

---
+++ .history/script_20211030141805.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+// https://restcountries.com/v2/
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v2/name/kenya');
+request.send();
+
+request.addEventListener('load', function () {
+  console.log(this.responseText);
+});
---
+++ .history/script_20211030141925.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+// https://restcountries.com/v2/
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v2/name/kenya');
+request.send();
+
+request.addEventListener('load', function () {
+  console.log(this.responseText);
+  const data = JSON.parse(this.responseText);
+  console.log(data);
+});
